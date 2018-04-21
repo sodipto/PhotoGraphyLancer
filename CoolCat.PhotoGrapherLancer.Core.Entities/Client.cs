350e8fd2339e84aa6c99ee38701044f79a80359d
Sodipto Saha <badhansaha444@gmail.com>
2018-04-21 14:37:53 +0600

    I update Client.cs  model.It,s ready for Client Model.

--- CoolCat.PhotoGrapherLancer.Core.Entities/Client.cs
+++ CoolCat.PhotoGrapherLancer.Core.Entities/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,48 @@ namespace CoolCat.PhotoGrapherLancer.Core.Entities
 
     public class Client
     {
+        [Key]
+        public int ClientId { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string Password { get; set; }
+
+
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password and password do not match")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
+
+
+
+
+        public int? Phone { get; set; }                 //Nullable Bydefault ?(nullable)
+
+        public string Address { get; set; }              //NullAble Bydefault 
+
+        public string ResetPasswordCode { get; set; }    // NullAble Bydefault 
+
+        public string ImagePath { get; set; }            // NullAble Bydefault 
+
+
     }
+
+
 
 
 
