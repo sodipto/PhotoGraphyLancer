094e1154e6472ba9c9b7d2fd6c339e03085c56d7
Sodipto Saha <badhansaha444@gmail.com>
2018-04-21 14:48:34 +0600

    Complete ClientLogin.cs Model..

--- CoolCat.PhotoGrapherLancer.Core.Entities/Client/ClientLogin .cs
+++ CoolCat.PhotoGrapherLancer.Core.Entities/Client/ClientLogin .cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,20 @@ namespace CoolCat.PhotoGrapherLancer.Core.Entities.Client
 {
     class ClientLogin
     {
+       
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }   //Optional ,If use Message Susscess or failed 
+
+    
     }
 }
