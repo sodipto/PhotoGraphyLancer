65a5aa75243d00ccd3057110a81d08f777a070b4
Sodipto Saha <badhansaha444@gmail.com>
2018-04-21 14:44:31 +0600

    I Add Client Folder For All The Client Entites model include here

--- CoolCat.PhotoGrapherLancer.Core.Entities/Client/Client.cs
+++ CoolCat.PhotoGrapherLancer.Core.Entities/Client/Client.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoolCat.PhotoGrapherLancer.Core.Entities
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.Client
 {
 
 
