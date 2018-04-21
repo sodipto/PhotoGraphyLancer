ab53c8e53aed799b6b6464857b49fddba1c7f7f0
Sodipto Saha <badhansaha444@gmail.com>
2018-04-21 16:34:26 +0600

    add PhotoGraphyCatagory.cs model which can be helped All the Project

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGraphyCatagory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities
+{
+    class PhotoGraphyCatagory
+    {
+        [Key]
+        public int CatagoryID { get; set; }
+
+        [Required]
+        public string  CatagoryName { get; set; }
+
+
+        public string CatagoryDescrip { get; set; }
+
+
+    }
+}
