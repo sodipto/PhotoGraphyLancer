3a59ba0f487e098869d59234b5935266750152e0
Sodipto Saha <badhansaha444@gmail.com>
2018-04-21 14:55:37 +0600

    Complete Client ResetPassword.cs Model With Reset Password Code

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/Client/ResetPassword.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.Client
+{
+    class ResetPassword
+    {
+        [Required(ErrorMessage = "New password required", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Minimum 6 characters required")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "New password and confirm password does not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ResetCode { get; set; }     //This Field Hidden For view PAge Come From DataBase
+    }
+}
