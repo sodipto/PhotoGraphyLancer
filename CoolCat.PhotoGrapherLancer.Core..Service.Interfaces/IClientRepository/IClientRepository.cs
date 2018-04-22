1c02307ba6871758b15956976ccc627f0434e00a
Sodipto Saha <badhansaha444@gmail.com>
2018-04-22 16:35:24 +0600

    I add IcientRepository

---
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IClientRepository/IClientRepository.cs
@@ -0,0 +1,34 @@
+﻿using CoolCat.PhotoGrapherLancer.Core.Entities.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IClientRepository
+{
+    public interface IClientRepository
+    {
+
+        //Client Profile
+
+        IEnumerable<Client> GetClient(int id);
+
+
+        //Add Client
+
+        bool AddClient(Client Newclient);
+
+        //edit view
+
+        Client EditView(int id);
+
+        //Edit
+
+        bool Edit(Client editclient);
+
+       //
+
+
+    }
+}
