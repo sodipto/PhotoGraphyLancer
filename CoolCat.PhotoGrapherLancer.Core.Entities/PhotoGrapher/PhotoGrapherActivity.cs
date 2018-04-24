d3c2ae2c4d850f2dcf7994aa31491533eabeebe6
Sodipto Saha <badhansaha444@gmail.com>
2018-04-25 00:37:25 +0600

    I Add PhotoGrapherActivity model

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PhotoGrapherActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PhotoGrapherActivity
+    {
+        [Key]
+        public int ActivityID     { get; set; }
+
+        public int PhotoGrapherId { get; set; }
+
+        public DateTime Date_time { get; set; }    //current date time when Activity Post
+
+        public string Description { get; set; }
+
+        public string ImagePath   { get; set; }
+
+        public int? Likes { get; set; }           
+
+
+
+    }
+}
