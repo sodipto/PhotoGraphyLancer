2072c37760bda64939ff3520a8b7c5baa68291f7
Sodipto Saha <badhansaha444@gmail.com>
2018-04-25 00:22:47 +0600

    I Add PhotoGraphy Type model

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PhotoGrapher_PhotoGraphy_Type.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PhotoGrapher_PhotoGraphy_Type
+    {
+
+        [Key]
+        public int TypeID { get; set; }
+
+
+        public int Fk_PhotoGrapher_ID { get; set; }
+
+
+        public string PhotoGraphyTypes { get; set; }
+    }
+}
