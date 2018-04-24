fbe4af46025c85aacee8b91ad16a4c167f561365
Sodipto Saha <badhansaha444@gmail.com>
2018-04-25 01:00:44 +0600

    I add A File Albam Photo model

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/AlbamPhoto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+    public class AlbamPhoto
+    {
+
+        [Key]
+        public int AlbamPhotoID { get; set; }
+
+
+        public int Fk_Albam_ID { get; set; }
+
+        public int Fk_PhotoGrapher_ID { get; set; }
+
+        public DateTime Date_time { get; set; }    //current date time when Activity Post
+
+        public string ImagePath { get; set; }      //possible null
+
+        public string Caption { get; set; }       //possible null
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
