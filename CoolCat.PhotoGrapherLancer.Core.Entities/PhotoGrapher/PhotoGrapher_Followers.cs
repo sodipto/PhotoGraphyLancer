8a092d1804e9ca7c2d4327eb5a1a1e91fe1243d7
Sodipto Saha <badhansaha444@gmail.com>
2018-04-24 23:15:14 +0600

    I add Followes Entites In the Photo Grapher Enities

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PhotoGrapher_Followers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PhotoGrapher_Followers
+    {
+
+        public int FollowID { get; set; }      //Increment Automatic
+
+        public int Followers { get; set; }     //By Default Null Accepted
+
+        public int Fk_PhotoGrapher_ID { get; set; }   //Foreign Key From PhotoGrapher.cs
+
+
+
+
+
+    }
+}
