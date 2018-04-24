58f8bac92d0e8bfb1ea9f496f9eff4529fc08afd
Sodipto Saha <badhansaha444@gmail.com>
2018-04-25 00:18:01 +0600

    I Add Skills Model

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PhotoGrapherSkills.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PhotoGrapherSkills
+    {
+        [Key]
+        public int SkillsID { get; set; }
+
+
+        public int Fk_PhotoGrapher_ID { get; set; }
+
+
+        public string Skills { get; set; }
+    }
+}
