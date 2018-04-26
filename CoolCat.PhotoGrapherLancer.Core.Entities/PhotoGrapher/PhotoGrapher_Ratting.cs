7123eb898828659f019287d6b743560fa8949c94
Sodipto Saha <badhansaha444@gmail.com>
2018-04-27 02:00:55 +0600

    I add PhotoGrapher Ratting

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PhotoGrapher_Ratting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PhotoGrapher_Ratting
+    {
+
+        [Key]
+        public int RattingID { get; set; }      //Increment Automatic
+
+        public double Ratting { get; set; }     //By Default Null Accepted
+
+        public int Fk_Client_id { get; set; }  //which person Ratting
+
+        public int Fk_PhotoGrapher_ID { get; set; }   //Foreign Key From PhotoGrapher.cs
+    }
+}
