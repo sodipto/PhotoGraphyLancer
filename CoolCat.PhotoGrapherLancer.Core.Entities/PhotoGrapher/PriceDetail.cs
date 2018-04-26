a3bb82b630394503d43853f75c9195088394c7f2
Sodipto Saha <badhansaha444@gmail.com>
2018-04-27 01:44:25 +0600

    I Add Price Detail Model for Photo Grapher Model Folder

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PhotoGrapher/PriceDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher
+{
+   public class PriceDetail
+    {
+        [Key]
+        public int PriceDetailId { get; set; }
+
+        public int Fk_PhotoGrapher_ID { get; set; }
+
+        [Required]
+        public String Catagory { get; set; }
+
+        [Required]
+        public String Details { get; set; }
+
+        [Required]
+        public String Price { get; set; }
+
+
+
+
+
+
+    }
+}
