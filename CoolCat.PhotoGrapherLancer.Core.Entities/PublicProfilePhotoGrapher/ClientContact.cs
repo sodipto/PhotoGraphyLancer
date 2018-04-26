bc0510f44a6a00c24cd7ce97ea1842ff4aee0298
Sodipto Saha <badhansaha444@gmail.com>
2018-04-27 01:02:50 +0600

    I add Client Contact File In the Public photoGrapher

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/PublicProfilePhotoGrapher/ClientContact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.PublicProfilePhotoGrapher
+{
+   public class ClientContact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [MinLength(11,ErrorMessage = "At Least 11"),MaxLength(14, ErrorMessage = "Maximum")]
+        public int? Phone { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Message is required")]
+        [MinLength(20,ErrorMessage = "Mimimum Type 20 character"),MaxLength(120, ErrorMessage = "Not Gretter Then 120 Character")]
+        public String Message { get; set; }
+
+
+
+
+
+
+    }
+}
