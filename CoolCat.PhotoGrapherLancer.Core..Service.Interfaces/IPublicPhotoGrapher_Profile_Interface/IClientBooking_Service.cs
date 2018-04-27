f4697c6f96a276d00d9ad6d91334ee7d36050e23
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 01:53:01 +0600

    Modify Booking Client File

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientBooking_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientBooking_Service.cs
@@ -1,4 +1,5 @@
-﻿using CoolCat.PhotoGrapherLancer.Core.Entities.PublicProfilePhotoGrapher;
+﻿using CoolCat.PhotoGrapherLancer.Core.Entities.Client;
+using CoolCat.PhotoGrapherLancer.Core.Entities.PublicProfilePhotoGrapher;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,7 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPublicPhotoGrapher
     {
 
 
-        //Get All Booking Show By Admin
-
-
-
+       
 
 
 
