2f6e769d1da13331f8070875d7820d770969d21f
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 01:15:46 +0600

    Modify

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientBooking_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientBooking_Service.cs
@@ -1,5 +1,4 @@
-﻿using CoolCat.PhotoGrapherLancer.Core.Entities.PublicProfilePhotoGrapher;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPublicPhotoGrapher_Profile_Interface
 {
-    public interface IClientContact_Service
+   public interface IClientBooking_Service
     {
-
 
 
     }
