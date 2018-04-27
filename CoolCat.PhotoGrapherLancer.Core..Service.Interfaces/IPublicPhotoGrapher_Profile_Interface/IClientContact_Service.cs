b45c25c8b9c14bd35bff6aeac6b2c088d3bae750
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 01:21:38 +0600

    Modify

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientContact_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPublicPhotoGrapher_Profile_Interface/IClientContact_Service.cs
@@ -13,7 +13,6 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPublicPhotoGrapher
 
         //All The Contact Get By Admin
 
-        IEnumerable<ClientContact> AllGetContact();
 
 
         //Get Contact By PhotoGrapher
