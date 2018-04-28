80960f81bbf2f85753b1405e54a223a53fa4bc4c
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 15:08:49 +0600

    Region Follower  Create And Modify

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoolCat.PhotoGrapherLancer.Core.Entities.PhotoGrapher;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServic
 {
    public interface IPhotoGrapher_Social_Service
     {
+
+        #region //This Region Basically Follower 
+
+        //Get All Follower List By PhotoGrapher ID
+
+        IEnumerable<PhotoGrapher_Follower> GetAll(int id); //Thsi id PhotoGrapher Id
+
+        //Count Total Followers
+         int TotalFollowers(int id);      //Thsi id PhotoGrapher Id
+
+
+        //Save Followers
+        bool AddFollowers(PhotoGrapher_Follower Flw); //Check Condition 1 Client only one time Follow
+
+        #endregion 
+
     }
 }
