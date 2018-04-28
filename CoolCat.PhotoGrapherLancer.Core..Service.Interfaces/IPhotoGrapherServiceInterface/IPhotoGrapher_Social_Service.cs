61763421e5cf5ef8414242f5ae14482711456e0d
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 14:54:30 +0600

    IPhotoGrapher_Social_Service Interface Add

---
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServiceInterface
+{
+   public interface IPhotoGrapher_Social_Service
+    {
+    }
+}
