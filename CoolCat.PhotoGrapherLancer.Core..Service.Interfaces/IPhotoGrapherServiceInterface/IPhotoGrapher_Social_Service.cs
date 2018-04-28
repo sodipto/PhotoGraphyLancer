1aea5811dfde7929bfa91b333548dd846dc93e4d
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 15:12:49 +0600

    Following Service Add

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
@@ -14,7 +14,7 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServic
 
         //Get All Follower List By PhotoGrapher ID
 
-        IEnumerable<PhotoGrapher_Follower> GetAll(int id); //Thsi id PhotoGrapher Id
+        IEnumerable<PhotoGrapher_Follower> GetAllFollowers(int id); //Thsi id PhotoGrapher Id
 
         //Count Total Followers
          int TotalFollowers(int id);      //Thsi id PhotoGrapher Id
@@ -23,7 +23,26 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServic
         //Save Followers
         bool AddFollowers(PhotoGrapher_Follower Flw); //Check Condition 1 Client only one time Follow
 
+        #endregion
+
+
+
+        #region //This Region Basically Following
+
+        //Get All Follower List By PhotoGrapher ID
+
+        IEnumerable<PhotoGrapher_Following> GetAllFollowing(int id); //Thsi id PhotoGrapher Id
+
+        //Count Total Followers
+        int TotalFollowing(int id);      //Thsi id PhotoGrapher Id
+
+
+        //Save Followers
+        bool AddFollowing(PhotoGrapher_Following Flow); //Check Condition 1 Client only one time Follow
+
         #endregion 
+
+
 
     }
 }
