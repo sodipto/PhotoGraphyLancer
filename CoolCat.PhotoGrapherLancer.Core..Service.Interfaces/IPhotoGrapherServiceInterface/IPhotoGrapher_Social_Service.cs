279a53ae9455201ea7d8d990e04a267355e77145
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 15:20:07 +0600

    PhotoGrapher Ratting Region Add

--- CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Social_Service.cs
@@ -13,15 +13,18 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServic
         #region //This Region Basically Follower 
 
         //Get All Follower List By PhotoGrapher ID
+        //Thsi id PhotoGrapher Id
 
-        IEnumerable<PhotoGrapher_Follower> GetAllFollowers(int id); //Thsi id PhotoGrapher Id
+        IEnumerable<PhotoGrapher_Follower> GetAllFollowers(int id);
 
         //Count Total Followers
-         int TotalFollowers(int id);      //Thsi id PhotoGrapher Id
+        //Thsi id PhotoGrapher Id
+        int TotalFollowers(int id);     
 
 
         //Save Followers
-        bool AddFollowers(PhotoGrapher_Follower Flw); //Check Condition 1 Client only one time Follow
+        //Check Condition 1 Client only one time Follow
+        bool AddFollowers(PhotoGrapher_Follower Flw); 
 
         #endregion
 
@@ -30,17 +33,39 @@ namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServic
         #region //This Region Basically Following
 
         //Get All Follower List By PhotoGrapher ID
-
-        IEnumerable<PhotoGrapher_Following> GetAllFollowing(int id); //Thsi id PhotoGrapher Id
+        //Thsi id PhotoGrapher Id
+        IEnumerable<PhotoGrapher_Following> GetAllFollowing(int id);
 
         //Count Total Followers
-        int TotalFollowing(int id);      //Thsi id PhotoGrapher Id
+        //Thsi id PhotoGrapher Id
+        int TotalFollowing(int id);    
 
 
-        //Save Followers
-        bool AddFollowing(PhotoGrapher_Following Flow); //Check Condition 1 Client only one time Follow
+        //Save Following
+        //Check Condition 1 Client only one time Follow
+        bool AddFollowing(PhotoGrapher_Following Flow);
+
+        #endregion
+
+
+        #region //This Region Basically Ratting
+
+        //count All The People Which Is Rating this PhotoGrapher
+        int Total_People_Ratting(int id);
+
+
+        //Count Total Followers
+        //Sum All Rating Point And Calculation And Return Value
+        //Thsi id PhotoGrapher Id
+        Double TotalRatting(int id);
+
+
+        //Save Ratting
+        //Check Condition 1 Client only one time Ratting Next Time Update
+        bool AddRatting(PhotoGrapher_Following Rat);
 
         #endregion 
+
 
 
 
