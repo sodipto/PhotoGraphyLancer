3983434c4e4d64cde6379b03145cd72ad82424b3
Sodipto Saha <badhansaha444@gmail.com>
2018-04-28 20:56:36 +0600

    I add  IPhotoGrapher_Album_Service

---
+++ CoolCat.PhotoGrapherLancer.Core..Service.Interfaces/IPhotoGrapherServiceInterface/IPhotoGrapher_Album_Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Service.Interfaces.IPhotoGrapherServiceInterface
+{
+    public interface IPhotoGrapher_Album_Service
+    {
+
+        
+
+
+
+
+    }
+}
