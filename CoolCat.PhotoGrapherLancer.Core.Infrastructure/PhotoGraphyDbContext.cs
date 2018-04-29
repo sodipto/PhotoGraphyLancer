52af92a43ac2055064ff39abc82b9f2149433475
Sodipto Saha <badhansaha444@gmail.com>
2018-04-29 13:45:07 +0600

    I Add New Project CoolCat.PhotoGrapherLancer.Core.Infrastructure Here All The Database Entity

---
+++ CoolCat.PhotoGrapherLancer.Core.Infrastructure/PhotoGraphyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoolCat.PhotoGrapherLancer.Core.Entities.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Infrastructure
+{
+    public class PhotoGraphyDbContext:DbContext
+    {
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ClientJobsPost> ClientJobsPosts { get; set;}
+
+
+
+    }
+}
