d1027968a7b718a657f0dcb09d5b26def5d9ec31
Sodipto Saha <badhansaha444@gmail.com>
2018-04-29 13:16:21 +0600

    I Add New Project CoolCat.PhotoGrapherLancer.Core._.Service

---
+++ CoolCat.PhotoGrapherLancer.Core..Service/ClientServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core._.Service
+{
+    public class ClientServices
+    {
+
+    }
+}
