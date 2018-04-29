b86eaccff52cfaf60672d01d944b3103d36e250f
Sodipto Saha <badhansaha444@gmail.com>
2018-04-29 19:51:37 +0600

    Add photoGrapher Profile View Model

---
+++ CoolCat.PhotoGrapherLancer.Core.Entities/ViewModel/PhotoGrapher_Profile_ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCat.PhotoGrapherLancer.Core.Entities.ViewModel
+{
+    public  class PhotoGrapher_Profile_ViewModel
+    {
+
+
+    }
+}
